b8770a2de5c7645d736697191e915fa4dcf9ad8b
david <dsissoko@gmail.com>
2025-08-14 11:48:32 +0200

    fixed router  avec création de la base au build et pas au run

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { createRouter } from '@generouted/react-router'
+import { Routes } from '@generouted/react-router'
 import { ThemeProvider, BaseStyles } from '@primer/react'
 
 import './reset.css'
 import './globals.css'
 
 import ColorModeSwitcher from './components/ColorModeSwitcher'
-
-const router = createRouter({
-    basename: import.meta.env.VITE_BASE_URL,
-})
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
@@ -21,7 +16,7 @@ root.render(
         <ThemeProvider colorMode="auto">
             <BaseStyles>
                 <ColorModeSwitcher />
-                <RouterProvider router={router} />
+                <Routes />
             </BaseStyles>
         </ThemeProvider>
     </StrictMode>
