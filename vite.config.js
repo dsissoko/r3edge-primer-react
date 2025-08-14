a068018387d09ae6508f5c2e653f14f0fde5a333
David Sissoko <dsissoko@users.noreply.github.com>
2025-08-14 09:24:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import browsers from '@github/browserslist-config'
 import { globSync } from 'glob'
 
 export default defineConfig({
+    base: '/r3edge-trading-app/',
     plugins: [react(), generouted()],
     server: { port: 1234 },
     css: {
