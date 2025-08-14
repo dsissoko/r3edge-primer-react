bf8af5b694e6dd7425be0bee792d39ab76f99232
david <dsissoko@gmail.com>
2025-08-14 11:25:48 +0200

    ajout d'une base pour support déploiement dans GITHUBPAGE

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import generouted from '@generouted/react-router/plugin'
 import postcssGlobalData from '@csstools/postcss-global-data'
@@ -6,30 +6,34 @@ import postcssPresetEnv from 'postcss-preset-env'
 import browsers from '@github/browserslist-config'
 import { globSync } from 'glob'
 
-export default defineConfig({
-    plugins: [react(), generouted()],
-    server: { port: 1234 },
-    css: {
-        postcss: {
-            plugins: [
-                postcssGlobalData({
-                    files: globSync(
-                        'node_modules/@primer/primitives/dist/css/**/*.css'
-                    ),
-                }),
-                postcssPresetEnv({
-                    stage: 2,
-                    browsers,
-                    // https://preset-env.cssdb.org/features/#stage-2
-                    features: {
-                        'nesting-rules': {
-                            noIsPseudoSelector: true,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        base: env.VITE_BASE_URL || '/',
+        plugins: [react(), generouted()],
+        server: { port: 1234 },
+        css: {
+            postcss: {
+                plugins: [
+                    postcssGlobalData({
+                        files: globSync(
+                            'node_modules/@primer/primitives/dist/css/**/*.css'
+                        ),
+                    }),
+                    postcssPresetEnv({
+                        stage: 2,
+                        browsers,
+                        // https://preset-env.cssdb.org/features/#stage-2
+                        features: {
+                            'nesting-rules': {
+                                noIsPseudoSelector: true,
+                            },
+                            'focus-visible-pseudo-class': false,
+                            'logical-properties-and-values': false,
                         },
-                        'focus-visible-pseudo-class': false,
-                        'logical-properties-and-values': false,
-                    },
-                }),
-            ],
+                    }),
+                ],
+            },
         },
-    },
+    }
 })
