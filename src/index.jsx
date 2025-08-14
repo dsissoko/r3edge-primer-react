5718db9336d538c2ad86546a1b4af000b8d6e279
david <dsissoko@gmail.com>
2025-08-14 11:54:54 +0200

    fixed router

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Routes } from '@generouted/react-router'
+import { RouterProvider } from 'react-router-dom'
+import { createRouter } from '@generouted/react-router/client'
 import { ThemeProvider, BaseStyles } from '@primer/react'
 
 import './reset.css'
 import './globals.css'
 
 import ColorModeSwitcher from './components/ColorModeSwitcher'
+
+const router = createRouter({
+    basename: import.meta.env.VITE_BASE_URL,
+})
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
@@ -16,7 +21,7 @@ root.render(
         <ThemeProvider colorMode="auto">
             <BaseStyles>
                 <ColorModeSwitcher />
-                <Routes />
+                <RouterProvider router={router} />
             </BaseStyles>
         </ThemeProvider>
     </StrictMode>
